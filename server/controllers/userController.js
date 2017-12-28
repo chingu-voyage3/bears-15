6db750c0bf35425eb99cbc75a38bd0589e1f9b82
bolu ajibs <bolu.ajibawo@gmail.com>
2017-12-28 12:26:35 +0100

    route to get sample data

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,14 @@
+const Demo = require('../models/Demo');
+
 exports.home = (req, res) => {
   res.send('hello world');
-}
+};
 
 exports.login = (req, res) => {
   res.send('is user logged in ?');
-}
+};
+
+exports.getSampleData = async (req, res) => {
+  const sampleData = await Demo.find({});
+  res.json(sampleData);
+};
