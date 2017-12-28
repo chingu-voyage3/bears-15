ef7a8cc63eb33820c566da92ebc0647a9b7fae90
bolu ajibs <bolu.ajibawo@gmail.com>
2017-12-28 12:06:09 +0100

    cleaned code

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const userController = require('../controllers/userController');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('hello world');
-});
+router.get('/', userController.home);
+router.get('/login', userController.login);
 
 module.exports = router;
