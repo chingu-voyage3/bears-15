87e8f2963d8ee41dbdefce418780e2943dcb4ace
bolu ajibs <bolu.ajibawo@gmail.com>
2017-12-29 14:15:38 +0100

    enabled cors and bodyparser

--- server/app.js
+++ server/app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const routes = require('./routes/index');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// enable cors
+const corsOptions = {
+  origin: true,
+  methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+  credentials: true,
+  exposedHeaders: ['x-auth-token']
+};
+app.use(cors(corsOptions));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 // load routes
-app.use('/', routes);
+app.use('/api/v1', routes);
 
 module.exports = app;
